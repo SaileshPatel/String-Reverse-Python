b9401a057bab0c7f05c9809adf5e2c42de7f8c22
SaileshPatel <saileshapatel@gmail.com>
2014-04-25 23:00:45 +0100

    completed program

--- ReverseString.py
+++ ReverseString.py
@@ -4,13 +4,29 @@ def dead(why):
 	exit(0)
 
 def start():
+	print("Select one of the two options!")
+	print("1. Lower Case")
+	print("2. Upper Case")
+	print("Press any other key to exit.")
+
+	option = input("> ")
+	if option == "1":
+		lowerCase()
+	elif option == "2":
+		upperCase()
+	else:
+		dead("Thank you for using ReverseString!")
 
 def lowerCase():
-	print("Enter the ")
+	print("Enter the string you wish to reverse.")
 	word = input("> ")
-	print(word[::-1].lower)
+	print(word[::-1].lower())
+	start()
 
 def upperCase():
-	print("")
+	print("Enter the string you wish to reverse.")
 	word = input("> ")
-	print(word[::-1].upper)
+	print(word[::-1].upper())
+	start()
+
+start()
